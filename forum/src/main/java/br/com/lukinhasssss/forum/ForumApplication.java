257d2d6e77ea6bc9b91b2ea1e7467022c44f44d9
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-21 20:22:11 -0300

    Add cache

--- forum/src/main/java/br/com/lukinhasssss/forum/ForumApplication.java
+++ forum/src/main/java/br/com/lukinhasssss/forum/ForumApplication.java
@@ -2,8 +2,12 @@ package br.com.lukinhasssss.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // Habilita o suporte para o Spring pegar da requisição dos parâmetros da URL os campos, informações de url (paginacao/ordenacao) e repassar isso para o Spring Data
+@EnableCaching // Habilita o uso de cache na aplicação
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- forum/src/main/java/br/com/lukinhasssss/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/lukinhasssss/forum/controller/TopicosController.java
@@ -8,10 +8,11 @@ import br.com.lukinhasssss.forum.model.Topico;
 import br.com.lukinhasssss.forum.repositories.CursoRepository;
 import br.com.lukinhasssss.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -32,9 +33,8 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao) {
-        Pageable paginacao = PageRequest.of(pagina, quantidade, Sort.Direction.DESC, ordenacao);
-
+    @Cacheable(value = "listaDeTopicos")
+    public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable paginacao) {
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
