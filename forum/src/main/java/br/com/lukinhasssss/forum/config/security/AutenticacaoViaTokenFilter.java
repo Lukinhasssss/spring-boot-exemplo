6b41e9fc6ea2faecfbdcd16bb78dacc520f0b9c6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-22 16:18:32 -0300

    Validating token

--- forum/src/main/java/br/com/lukinhasssss/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/lukinhasssss/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -9,12 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {  // É um filtro do Spring que é chamado uma única vez a cada requisição
+    // Observação --> Nos filters não é possível fazer injeção de dependências
+
+    private TokenService tokenService;
+
+    public AutenticacaoViaTokenFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Recuperar o token do header, validar, autenticar o usuário
         String token = recuperarToken(request);
-        System.out.println(token);
+        boolean valido = tokenService.isTokenValido(token);
+        System.out.println(valido);
 
         filterChain.doFilter(request, response);
     }
