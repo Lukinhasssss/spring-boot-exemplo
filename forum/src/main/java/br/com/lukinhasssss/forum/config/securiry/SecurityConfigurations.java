6fa0600eccfb743613c404e82497c8fadac31ea7
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-06-22 09:10:08 -0300

    Releasing access to public endpoints

--- forum/src/main/java/br/com/lukinhasssss/forum/config/securiry/SecurityConfigurations.java
+++ forum/src/main/java/br/com/lukinhasssss/forum/config/securiry/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.lukinhasssss.forum.config.securiry;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
-    
+    // Serve para configurar a parte de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
+
+    // Serve para fazer configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    // Serve para fazer configurações de recursos estáticos (js, css, imagens, etc.)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
